0dcdd17ebeea2cf0c2267a3e1326f868347b9e81
GuilhermeDGDEV <guilhermediasgoncalvesdev@gmail.com>
2022-11-11 22:51:43 -0300

    Login

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js';
+import { getAuth, signInWithEmailAndPassword, signOut } from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js';
 
 const firebaseConfig = {
@@ -14,3 +14,39 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+
+var user = null;
+const formLogin = document.querySelector('form');
+const loginArea = document.querySelector('#login');
+const logout = document.querySelector('#logout');
+const containerLogado = document.querySelector('#container-login');
+
+formLogin.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = document.querySelector('[name=email]').value;
+    const password = document.querySelector('[name=password]').value;
+    login(email, password);
+});
+
+logout.addEventListener('click', e => {
+    e.preventDefault();
+    signOut(auth)
+        .then(() => {
+            alert('Deslogado')
+            loginArea.style.display = 'block';
+            containerLogado.style.display = 'none';
+        })
+        .catch(error => alert(error.message));
+});
+
+function login(email, password) {
+    signInWithEmailAndPassword(auth, email, password)
+        .then(userCredential => {
+            user = userCredential.user;
+            alert('Logado com sucesso!');
+            loginArea.style.display = 'none';
+            containerLogado.style.display = 'block';
+            formLogin.reset();
+        })
+        .catch(error => alert(error.message));
+}
